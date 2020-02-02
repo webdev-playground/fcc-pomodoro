47291dea86d34a98d2850eb009b7221108f70a0c
Josh Choo <dev@joshuous.com>
2020-02-03 00:03:27 +0800

    implement Duration components and render them

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './index.scss';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,13 +12,66 @@ class App extends React.Component {
   }
 
   render() {
-    return <div className="app">App</div>;
+    return (
+      <div className="app">
+        <h1 className="app__title">Pomodoro Clock</h1>
+        <Duration
+          label="Break Length"
+          value={5}
+          labelID="break-label"
+          decrementID="break-decrement"
+          incrementID="break-increment"
+          durationID="break-length"
+        />
+        <Duration
+          label="Session Length"
+          value={25}
+          labelID="session-label"
+          decrementID="session-decrement"
+          incrementID="session-increment"
+          durationID="session-length"
+        />
+      </div>
+    );
   }
 }
 
-function Duration(props) {
-  return <div className="duration">Duration</div>;
+function Duration({
+  label,
+  value,
+  labelID,
+  decrementID,
+  incrementID,
+  durationID,
+}) {
+  return (
+    <div className="duration">
+      <div id={labelID} className="duration__label">
+        {label}
+      </div>
+      <div className="duration__controls">
+        <button id={decrementID} className="duration__button" type="submit">
+          -
+        </button>
+        <div id={durationID} className="duration__value">
+          {value}
+        </div>
+        <button id={incrementID} className="duration__button" type="submit">
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
+
+Duration.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  labelID: PropTypes.string.isRequired,
+  decrementID: PropTypes.string.isRequired,
+  incrementID: PropTypes.string.isRequired,
+  durationID: PropTypes.string.isRequired,
+};
 
 function Countdown(props) {
   return <div className="countdown">Countdown</div>;
