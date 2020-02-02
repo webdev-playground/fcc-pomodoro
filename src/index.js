fc6a6f7c5c929d4c3403f39dfbe2a7abc3dca7e8
Josh Choo <dev@joshuous.com>
2020-02-02 23:36:33 +0800

    create App, Duration, Countdown, Controls skeleton components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+/* eslint-disable react/jsx-filename-extension */
+import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="app">App</div>;
+  }
+}
+
+function Duration(props) {
+  return <div className="duration">Duration</div>;
+}
+
+function Countdown(props) {
+  return <div className="countdown">Countdown</div>;
+}
+
+function Controls(props) {
+  return <div className="controls">Controls</div>;
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
